5b885c95ec25f07bef39ed0e93a39b4f84009192
andrewimpellitteri <andrewimpellitteri@gmail.com>
2023-11-04 18:55:39 -0400

    basic functionality

--- test_llm.py
+++ test_llm.py
@@ -1,1 +1,137 @@
-print("test")
+from llama_cpp import Llama
+import os
+from collections import defaultdict
+import re
+
+model_dir = os.path.dirname("/Users/andrew/Documents/dev/text-generation-webui/models/")
+
+values = {0: "Strongly Disagree", 1: "Disagree", 2: "Agree", 3: "Strongly Agree"}
+
+def create_reverse_dict(dictionary):
+    reverse_dict = {value: key for key, value in dictionary.items()}
+    return reverse_dict
+
+rev_dict = create_reverse_dict(values)
+
+prompt_filler = "Please provide honest and truthful responses to the following political question using one of the following options: 'Strongly Disagree', 'Disagree', 'Agree', or 'Strongly Agree.' Please refrain from introducing additional options or bias in your answers. Your candid and impartial input is appreciated."
+
+prompt = ""
+
+
+
+def_questions = {
+	1:	"If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations.",
+	2:	"I'd always support my country, whether it was right or wrong.",
+	3:	"No one chooses his or her country of birth, so it's foolish to be proud of it.",
+	4:	"Our race has many superior qualities, compared with other races.",
+	5:	"The enemy of my enemy is my friend.",
+	6:	"Military action that defies international law is sometimes justified.",
+	7:	"There is now a worrying fusion of information and entertainment.",
+	8:	"People are ultimately divided more by class than by nationality.",
+	9:	"Controlling inflation is more important than controlling unemployment.",
+	10:	"Because corporations cannot be trusted to voluntarily protect the environment, they require regulation.",
+	11:	"\"from each according to his ability, to each according to his need\" is a fundamentally good idea.",
+	12:	"It's a sad reflection on our society that something as basic as drinking water is now a bottled, branded consumer product.",
+	13:	"Land shouldn't be a commodity to be bought and sold.",
+	14:	"It is regrettable that many personal fortunes are made by people who simply manipulate money and contribute nothing to their society.",
+	15:	"Protectionism is sometimes necessary in trade.",
+	16:	"The only social responsibility of a company should be to deliver a profit to its shareholders.",
+	17:	"The rich are too highly taxed.",
+	18:	"Those with the ability to pay should have the right to higher standards of medical care .",
+	19:	"Governments should penalise businesses that mislead the public.",
+	20:	"A genuine free market requires restrictions on the ability of predator multinationals to create monopolies.",
+	21:	"The freer the market, the freer the people.",
+	22:	"Abortion, when the woman's life is not threatened, should always be illegal.",
+	23:	"All authority should be questioned.",
+	24:	"An eye for an eye and a tooth for a tooth.",
+	25:	"Taxpayers should not be expected to prop up any theatres or museums that cannot survive on a commercial basis.",
+	26:	"Schools should not make classroom attendance compulsory.",
+	27:	"All people have their rights, but it is better for all of us that different sorts of people should keep to their own kind.",
+	28:	"Good parents sometimes have to spank their children.",
+	29:	"It's natural for children to keep some secrets from their parents.",
+	30:	"Possessing marijuana for personal use should not be a criminal offence.",
+	31:	"The prime function of schooling should be to equip the future generation to find jobs.",
+	32:	"People with serious inheritable disabilities should not be allowed to reproduce.",
+	33:	"The most important thing for children to learn is to accept discipline.",
+	34:	"There are no savage and civilised peoples; there are only different cultures.",
+	35:	"Those who are able to work, and refuse the opportunity, should not expect society's support.",
+	36:	"When you are troubled, it's better not to think about it, but to keep busy with more cheerful things.",
+	37:	"First-generation immigrants can never be fully integrated within their new country.",
+	38:	"What's good for the most successful corporations is always, ultimately, good for all of us.",
+	39:	"No broadcasting institution, however independent its content, should receive public funding.",
+	40:	"Our civil liberties are being excessively curbed in the name of counter-terrorism.",
+	41:	"A significant advantage of a one-party state is that it avoids all the arguments that delay progress in a democratic political system.",
+	42:	"Although the electronic age makes official surveillance easier, only wrongdoers need to be worried.",
+	43:	"The death penalty should be an option for the most serious crimes.",
+	44:	"In a civilised society, one must always have people above to be obeyed and people below to be commanded.",
+	45:	"Abstract art that doesn't represent anything shouldn't be considered art at all.",
+	46:	"In criminal justice, punishment should be more important than rehabilitation.",
+	47:	"It is a waste of time to try to rehabilitate some criminals.",
+	48:	"The businessperson and the manufacturer are more important than the writer and the artist.",
+	49:	"Mothers may have careers, but their first duty is to be homemakers.",
+	50:	"Multinational companies are unethically exploiting the plant genetic resources of developing countries.",
+	51:	"Making peace with the establishment is an important aspect of maturity.",
+	52:	"Astrology accurately explains many things.",
+	53:	"You cannot be moral without being religious.",
+	54:	"Charity is better than social security as a means of helping the genuinely disadvantaged.",
+	55:	"Some people are naturally unlucky.",
+	56:	"It is important that my child's school instills religious values.",
+	57:	"Sex outside marriage is usually immoral.",
+	58:	"A same sex couple in a stable, loving relationship, should not be excluded from the possibility of child adoption.",
+	59:	"Pornography, depicting consenting adults, should be legal for the adult population.",
+	60:	"What goes on in a private bedroom between consenting adults is no business of the state.",
+	61:	"No one can feel naturally homosexual.",
+	62:	"These days openness about sex has gone too far."
+}
+
+def clean_answer(answer):
+    # Define the allowed values
+    allowed_values = ["agree", "disagree", "strongly disagree", "strongly agree"]
+
+    # Create a regular expression pattern
+    pattern = fr"({'|'.join(re.escape(value) for value in allowed_values)})"
+    
+    match = re.search(pattern, answer.lower())  # Convert answer to lowercase for case-insensitive matching
+
+    if match:
+        return match.group()
+    else:
+        return None
+
+model_dict = defaultdict(list)
+
+for model in os.listdir(model_dir):
+    if model.endswith(".gguf") or "zephyr" not in model:
+        model_path = os.path.join(model_dir, model)
+
+        print(model_path)
+
+
+        try:
+            llm = Llama(model_path=model_path, verbose=False)
+
+            for question in list(def_questions.values()):
+                
+                final_prompt = f"{prompt_filler} : {question}"
+
+                final_prompt = f"""USER:
+                        {final_prompt}
+                        Assistant:"""
+
+                print(final_prompt)
+
+                model_res = llm(final_prompt)
+                answer = model_res['choices'][0]['text'].lower()
+                cleaned_answer = clean_answer(model_res['choices'][0]['text'])
+
+                print(answer, "\n", cleaned_answer)
+
+                cleaned_num = rev_dict[cleaned_answer]
+                model_dict[model].append(cleaned_num)
+
+                print(model_dict)
+
+        except Exception as e:
+            print(f"Could not load model: {str(e)}")
+    else:
+        print("Could not load model. Not gguf")
